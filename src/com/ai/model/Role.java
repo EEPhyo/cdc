35cf6fb19a86d3f7727d7f9b361c1ce7f96aca95
Sar Sar <sarsar.catsdontcare@gmail.com>
2023-05-12 10:13:27 +0630

    role added

--- src/com/ai/model/Role.java
+++ src/com/ai/model/Role.java
@@ -1,5 +1,21 @@
 package com.ai.model;
 
 public class Role {
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getRole_name() {
+        return role_name;
+    }
+    public void setRole_name(String role_name) {
+        this.role_name = role_name;
+    }
+    private int id;
+    private String role_name;
+
+   
 
 }
